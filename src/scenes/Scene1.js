b0216b85f45709a91f08c1b22bb50a85ae373bd1
anahit <sathytrench@gmail.com>
2022-04-19 16:35:16 -0400

    ties camera movement to player and adds in house and door

--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -15,8 +15,10 @@ export default class Scene1 extends Phaser.Scene {
       this.load.image('sky', 'assets/sky.png');
       //sprites 
       this.load.image('ground', 'assets/ground.png');
+      this.load.image('house', 'assets/greenhouse.png');
+      this.load.image('door', 'assets/door.png');
       //player
-      this.load.spritesheet('lady', 'assets/lady2.png', { frameWidth: 273.8, frameHeight: 600 });
+      this.load.spritesheet('lady', 'assets/lady.png', { frameWidth: 273.8, frameHeight: 600 });
     }
 
     //////////////////////////////////////////CREATE///////////////////////////////////////////////////////
@@ -25,14 +27,26 @@ export default class Scene1 extends Phaser.Scene {
       //background
       this.add.image(400, 300, 'sky');
 
+      //house
+      this.add.image(1680, 75, 'house');
+      this.add.image(1715, 300, 'door').setScale(0.4);
+
       //ground
       this.groundGroup = this.physics.add.staticGroup({classType: Ground}); 
-      this.groundGroup.create(400, 850, 'ground').refreshBody();
+      this.groundGroup.create(400, 775, 'ground').refreshBody();
     
       //player
       this.player = new Player(this, 100, 0, 'lady').setScale(0.65);
       this.player.setBounce(0.2);
       this.player.setCollideWorldBounds(true);
+
+      // set the boundaries of our game world
+      this.physics.world.bounds.width = 1800;
+
+      // set bounds so the camera won't go outside the game world
+      this.cameras.main.setBounds(0, 0, 1800);
+      // make the camera follow the player
+      this.cameras.main.startFollow(this.player);
 
       this.anims.create({
           key: 'left',
