9e5fb35d9cf0e5bc45150799386d2e2950da218b
anahit <sathytrench@gmail.com>
2022-04-19 16:59:06 -0400

    add full webpack and tiny server to get ready for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,45 @@
+'use strict'
+
+const isDev = process.env.NODE_ENV === 'development';
+
 const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
+  mode: isDev ? 'development' : 'production',
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/public/',
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'source-map',
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  module: {
+    // rules: [
+    //   //   {
+    //   //     test: [ /\.vert$/, /\.frag$/ ],
+    //   //     use: 'raw-loader'
+    //   //   }
+    // ],
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
 
-    entry: './src/index.js',
-
-    devServer: {
-      contentBase: './public',
-      inline: true,
-      hot: false
-    },
-
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        publicPath: '/public/',
-        filename: 'bundle.js'
-    },
-
-    module: {
-        rules: [
-          {
-            test: [ /\.vert$/, /\.frag$/ ],
-            use: 'raw-loader'
-          }
-        ]
-    },
-
-    plugins: [
-        new webpack.DefinePlugin({
-            'CANVAS_RENDERER': JSON.stringify(true),
-            'WEBGL_RENDERER': JSON.stringify(true)
-        })
-    ]
-
+  plugins: [
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true),
+    }),
+  ],
 };
